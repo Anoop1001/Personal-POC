fe0674fa3b4854b92fc59bfb89c64fe97f821ed6
Anoop H.N <anoop.10.01.91@gmail.com>
2021-10-05 17:45:21 +0530

    SqLite Implemented with CRUD Operations
    SqLite Implemented.
    Microsoft Dependency Implemented.
    Bindings for Textbox, Editor, Label and Button.

--- FirstApp/FirstApp/FirstApp/ViewModels/BaseViewModel.cs
+++ FirstApp/FirstApp/FirstApp/ViewModels/BaseViewModel.cs
@@ -1,31 +1,12 @@
-﻿using FirstApp.Models;
-using FirstApp.Services;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using Xamarin.Forms;
 
 namespace FirstApp.ViewModels
 {
     public class BaseViewModel : INotifyPropertyChanged
     {
-        public IDataStore<Item> DataStore => DependencyService.Get<IDataStore<Item>>();
-
-        bool isBusy = false;
-        public bool IsBusy
-        {
-            get { return isBusy; }
-            set { SetProperty(ref isBusy, value); }
-        }
-
-        string title = string.Empty;
-        public string Title
-        {
-            get { return title; }
-            set { SetProperty(ref title, value); }
-        }
-
         protected bool SetProperty<T>(ref T backingStore, T value,
             [CallerMemberName] string propertyName = "",
             Action onChanged = null)
---
+++ FirstApp/FirstApp/FirstApp/ViewModels/FirstAppViewModel.cs
@@ -0,0 +1,73 @@
+﻿using FirstApp.Contract;
+using FirstApp.Data.Models;
+using System;
+using System.Linq;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace FirstApp.ViewModels
+{
+    public partial class FirstAppViewModel : BaseViewModel
+    {
+        private readonly IDatabaseManager _dataBaseManager;
+        public FirstAppViewModel(IDatabaseManager dataBaseManager)
+        {
+            _dataBaseManager = dataBaseManager;
+            ClickAddCommand = new Command(AddButtonClicked());
+            ClickDisplayCommand = new Command(DisplayButtonClicked());
+            ClickClearCommand = new Command(ClearButtonClicked());
+        }
+
+        private Action<object> AddButtonClicked()
+        {
+            return async (e) =>
+            {
+                await _dataBaseManager.InsertData(new PersonalInformation { Name = Name, Address = Address });
+                ClickDisplayCommand.Execute(null);
+            };
+        }
+
+        private Action<object> DisplayButtonClicked()
+        {
+            return async (e) =>
+            {
+                var personalData = await _dataBaseManager.GetAllData<PersonalInformation>();
+                PersonalInformation = string.Join(Environment.NewLine, personalData.Select(x => $"{x.Name} {x.Address}"));
+            };
+        }
+
+        private Action<object> ClearButtonClicked()
+        {
+            return async (e) =>
+            {
+                await _dataBaseManager.ClearData<PersonalInformation>();
+                ClickDisplayCommand.Execute(null);
+            };
+        }
+
+        public ICommand ClickAddCommand { get; }
+        public ICommand ClickDisplayCommand { get; }
+        public ICommand ClickClearCommand { get; }
+
+        string personalInformation = string.Empty;
+        public string PersonalInformation
+        {
+            get { return personalInformation; }
+            set { SetProperty(ref personalInformation, value); }
+        }
+
+        string name = string.Empty;
+        public string Name
+        {
+            get { return name; }
+            set { SetProperty(ref name, value); }
+        }
+
+        string address = string.Empty;
+        public string Address
+        {
+            get { return address; }
+            set { SetProperty(ref address, value); }
+        }
+    }
+}
